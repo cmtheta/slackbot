de794b2f7b37a1ba70e39d60ff9c6850df5af448
junno <neneloophost@gmail.com>
2023-04-28 21:01:51 +0000

    commit

---
+++ slackbot/__main__.py
@@ -0,0 +1,60 @@
+import os
+import logging
+from slack_bolt import App
+from slack_sdk import WebClient
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+
+SLACK_BOT_TOKEN = os.getenv("SLACK_BOT_TOKEN")
+SLACK_APP_TOKEN = os.getenv("SLACK_APP_TOKEN")
+
+CHANNEL_ID = os.getenv("CHANNEL_ID")
+
+app = App(token=SLACK_BOT_TOKEN)
+client = WebClient(token=SLACK_BOT_TOKEN)
+
+logging.basicConfig(
+    filename="logging.log",
+    level=logging.DEBUG,
+    format="(%(asctime)s)[%(levelname)s] %(message)s",
+    datefmt="%m/%d/%Y %I:%M:%S %p"
+)
+
+@app.event("channel_created")
+def chennel_created(event):
+    post_channel = CHANNEL_ID
+    channel_name = event['channel']["name"]
+    text = f"チャンネル #{channel_name} が作成されました。"
+    blocks = [
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": text
+			}
+		}
+	]
+    try:
+        client.chat_postMessage(
+            channel=post_channel,
+            text=text,
+            blocks=blocks
+        )
+    except Exception as e:
+        error_message()
+        logging.exception('Raise Exception: %s', e)
+
+def error_message():
+    post_channel = CHANNEL_ID
+    try:
+        client.chat_postMessage(
+            channel=post_channel,
+            text="処理エラーが発生しました。",
+        )
+    except Exception as e:
+        logging.exception('Raise Exception: %s', )
+
+
+handler = SocketModeHandler(app, SLACK_APP_TOKEN)
+handler.start()
+
+
